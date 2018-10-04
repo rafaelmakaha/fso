95cfc903a901dbea1fbd5aba809de902f31d5d08
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2018-10-04 17:05:28 -0300

    Adds random points generator.
    Signed-off-by: Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>

--- ex-04-22.c
+++ ex-04-22.c
@@ -1,17 +1,30 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 typedef struct Ponto{
-    int x;
-    int y;
+    float x;
+    float y;
 }ponto;
 
 int main(){
-    ponto  list[20];
+    ponto  list[100];
     int i=0;
-
+    int list_len = (sizeof(list)/sizeof(list[0]));
+    int rand_max = 2.00;
     
-    for(i = 0; i < (sizeof(list)/sizeof(list[0])); i++ ){
+    for(i = 0; i < list_len; i++ ){
         printf("%d\n", i);
+        if(rand()%2 == 0){
+            list[i].x = ((float)rand()/(float)(RAND_MAX) * rand_max);
+        }else{
+            list[i].x = -((float)rand()/(float)(RAND_MAX) * rand_max);            
+        }
+        if(rand()%2 == 0){
+            list[i].y = ((float)rand()/(float)(RAND_MAX) * rand_max);
+        }else{
+            list[i].y = -((float)rand()/(float)(RAND_MAX) * rand_max);
+        }
+        printf("X: %f; Y: %f\n", list[i].x, list[i].y);
     }
     
 
