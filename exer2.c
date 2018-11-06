e83a13771d2455c6456384adafe4a48855504910
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2018-11-06 11:06:31 -0200

    Adds dinamic queue test file
    Signed-off-by: Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
    
    Co-authored-by: João Vitor <joao.lemos4000@gmail.com>

--- exer2.c
+++ exer2.c
@@ -1,62 +1,60 @@
 #include <stdio.h>
+#include<stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
 
-sem_t semaf;
+typedef struct Fila{
+    int valor;
+    struct Fila* prox;
+}Queue;
 
-int nAlunos,cCadeiras;
-int *count;
-int to_naFila = 1;
+typedef struct Cab{
+    int quant;
+    struct Fila* inicio;
+    struct Fila* fim;
+}Indexes;
 
+Indexes *cab;
+int capacidade = 5;
 
-int set_alunos(){
-  int alunos = (rand()%40)+3;
-  return alunos;
+int fila_cheia(){
+    return (cab->quant == capacidade);
 }
 
-void *runner1(int count[]){
-  //função do assistente
-  sem_wait(&semaf);
-
-
-
-  sem_post(&semaf);
+int fila_vazia(){
+    return(cab->quant==0);
 }
 
-void *runner2(void *params){
-  int flag=3;
 
-  while(to_naFila == 1){
-    // Variavel que o monitor vai usar para chamar proximo aluno
-    // verifica propria posição na fila e se o monitor está disponivel
-  }
+void fila_insere(int valor) {
+    Queue *novo = (Queue *)malloc(sizeof(Queue));
+    novo->valor = valor;
+    novo->prox = NULL;
 
-  //função dos alunos
+    if(fila_cheia()){
+        return ;
+    }
+    if(fila_vazia()){
+        cab->inicio = novo;
+        cab->fim = novo;
+        cab->quant++;
+    }else{
+      cab->fim->prox = novo;
+      cab->fim = novo;
+      cab->quant ++;
+    }
 }
-
-void *runner3(void *params){
-
-}
-
 
 int main(){
+  // Inicia Fila e Cab
+  cab = malloc(sizeof(Indexes));
+  cab->inicio = NULL;
+  cab->fim = NULL;
+  cab->quant = 0;
 
-  nAlunos = set_alunos();
-  cCadeiras =nAlunos/2;
+  fila_insere(1);
+  fila_insere(2);
+  fila_insere(3);
 
-  pthread_t monitor;
-  pthread_t *aluno = malloc(sizeof(pthread_t)* nAlunos);
-
-  pthread_create(&monitor,NULL,runner1(&count[n]),NULL);
-  pthread_join(monitor,NULL);
-
-  for(int i=0;i<n;i++){
-    pthread_create(&aluno[i],NULL,runner2,NULL);
-  }
-  for(int i=0;i<n;i++){
-    pthread_join(aluno[i],NULL);
-  }
-
-  free(aluno);
-  return 0;
+  free(cab);
 }
